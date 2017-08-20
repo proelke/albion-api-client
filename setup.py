5103fd102ff812b376942ba22a311d628820177d
proelke <proelke@gmail.com>
2017-08-20 23:43:48 +0200

    removing long description

--- setup.py
+++ setup.py
@@ -2,21 +2,11 @@
 
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(
     name='albion-api-client',
-    version='0.1.0',
+    version='0.1.1',
     description='An API client for Albion Online written in Python.',
-    long_description=long_description,
     url='https://github.com/proelke/albion-api-client',
     author='Patrick Roelke',
     author_email='proelke@gmail.com',
